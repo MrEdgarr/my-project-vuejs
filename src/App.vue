d1704a88bc09bb49a82dd2f887891634ee066dba
MrEdgar <quanghuy.nguyen@vnlink.vn>
2023-02-27 10:24:57 +0700

    update file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from "vue-router"
-import TheFooter from "./components/layouts/theFooter.vue"
+import TheFooter from "./components/layouts/TheFooter.vue"
 import TheHeader from "./components/layouts/TheHeader.vue"
 </script>
 
